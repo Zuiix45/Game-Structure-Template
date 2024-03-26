703e33b5504d0cfc0853450d00b2cd05cfce1d64
Zuiix45 <kesikbas.kerem1@gmail.com>
2024-03-26 22:48:24 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,6 @@ int main(int argc, char* args[]) {
   	App::initApp("Game", "0.0.0", 800, 600, "./res/", "anta_regular", 32, args);
 
 	// init game
-	physics::setGravity(0.001f);
-
-	unsigned int pid = engine::registerObject(1, "player", make<Player>(0, 0, 80, 80, 0.5));
-
-	unsigned int aid = engine::registerObject(2, "a", make<Object>(ObjectType::OBJECT, 100, 100, 100, 100, 0));
-	engine::getObject(aid)->setAllColors(255, 255, 255, 1);
-	engine::getObject(aid)->closeAnimation();
-	engine::getObject(aid)->effectByCamera(true);
 	
 	App::startLoop(0);
 
