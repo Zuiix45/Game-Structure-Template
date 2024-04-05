e80b795280f9c7deafb1043d016bf7243d2e4049
Zuiix45 <kesikbas.kerem1@gmail.com>
2024-04-05 12:18:38 +0300

    vs gitignore

--- src/sys/Logger.h
+++ src/sys/Logger.h
@@ -22,24 +22,56 @@
 typedef int ErrorCode;
 
 namespace logger {
-    // use logPoint() instead
+    
+    /**
+     * @brief Logs the point of execution in the code.
+     * 
+     * @param file The name of the source file where the log point is called.
+     * @param line The line number in the source file where the log point is called.
+     */
     void _logPoint(const char* file, int line);
 
-    // use logError(error, code) instead
+    /**
+     * @brief Logs an error message with an optional error code.
+     * 
+     * @param file The name of the source file where the error is logged.
+     * @param line The line number in the source file where the error is logged.
+     * @param error The error message to be logged.
+     * @param code The error code associated with the error (default: NO_ERROR_CODE).
+     */
     void _logError(const char* file, int line, const std::string& error, ErrorCode code = NO_ERROR_CODE);
 
-    // use logInfo(info) instead
+    /**
+     * @brief Logs an informational message.
+     * 
+     * @param info The informational message to be logged.
+     */
     void _logInfo(const std::string& info);
 
+    /**
+     * @brief Prints the help information.
+     */
     void printHelp();
+
+    /**
+     * @brief Prints the version information.
+     * 
+     * @param name The name of the application.
+     * @param version The version of the application.
+     */
     void printVersion(const char* name, const char* version);
 
+    /**
+     * @brief Prompts the user to enter a value before closing the application.
+     * 
+     * @return The value entered by the user.
+     */
     int enterBeforeClose();
 
     /**
-     * @brief set logger for logging into a file. Useful on debug mode
+     * @brief Sets the logger to log into a file.
      * 
-     * @param outFile set false to prevent logger logging into a file
+     * @param outFile Set to false to prevent the logger from logging into a file.
      */
     void setOutLogFile(bool outFile);
 }
